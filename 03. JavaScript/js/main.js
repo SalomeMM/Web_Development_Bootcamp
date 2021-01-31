351160ac16fd3da4c4feb6880d3eebb718a2bcd5
SalomeMM <sal.mm3@gmail.com>
2021-01-31 18:35:03 +0100

    function with math floor

--- 03. JavaScript/js/main.js
+++ 03. JavaScript/js/main.js
@@ -158,6 +158,36 @@ function getBeer (bottles) {
 }
 getBeer(3)
 
+// Challenge: create a function to buy milk worth "x" amount of money.
+
+function getMilk(money) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    var numberOfBottles = Math.floor(money / 1.5);
+    console.log("buy " + numberOfBottles + " bottles of milk")
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+  getMilk(5)
+
+
+
+
+
 // function getMilk() {   
 //     console.log("leaveHouse");
 //     console.log("moveRight");

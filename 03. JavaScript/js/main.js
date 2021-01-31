eb4c22bff77f915a4c6eaf641018aa97fbce2848
SalomeMM <sal.mm3@gmail.com>
2021-01-31 19:36:32 +0100

    bmi calculator

--- 03. JavaScript/js/main.js
+++ 03. JavaScript/js/main.js
@@ -256,41 +256,16 @@ function getMilk2(money, costPerBottle) {
 
 //Create your function below this line.
 //The first parameter should be the weight and the second should be the height.
+// If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+// var bmi = bmiCalculator(65, 1.8); 
+// bmi should equal 20 when it's rounded to the nearest whole number.
+
 
 function bmiCalculator (weight, height) {
-    var bmiMath = Math.floor(weight / (height * height))
-    return bmiMath;
-}
+    var bmi = weight / Math.pow(height, 2);
+    return Math.round(bmi);
+} // we round the bmi after calculating it, in case we want to use the full bmi somewhere else.
 
-console.log (bmiCalculator(65, 1.8))
+var bmi = bmiCalculator(65, 1.8);
 
-/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
-var bmi = bmiCalculator(65, 1.8); 
-bmi should equal 20 when it's rounded to the nearest whole number.
-*/
-
-
-
-
-
-// function getMilk() {   
-//     console.log("leaveHouse");
-//     console.log("moveRight");
-//     console.log("moveRight");
-//     console.log("moveUp");
-//     console.log("moveUp");
-//     console.log("moveUp");
-//     console.log("moveUp");
-//     console.log("moveRight");
-//     console.log("moveRight");
-//     console.log("moveLeft");
-//     console.log("moveLeft");
-//     console.log("moveDown");
-//     console.log("moveDown");
-//     console.log("moveDown");
-//     console.log("moveDown");
-//     console.log("moveLeft");
-//     console.log("moveLeft");
-//     console.log("enterHouse");
-//   }
-
+console.log (bmi); // logs 20, as requested

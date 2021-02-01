80c78ed55efae49f1b53f19d9cf7c2373d5c6857
SalomeMM <sal.mm3@gmail.com>
2021-02-01 15:32:48 +0100

    bmi calculator

--- 04. JavaScript - Intermediate/main.js
+++ 04. JavaScript - Intermediate/main.js
@@ -29,3 +29,25 @@ if (loveScore > 30 && loveScore <= 70) {
 if (loveScore <= 30) {
     alert("Your love score is " + loveScore + "%." + " You get together like oil and water")
     }
+
+
+// --- Challenge: advanced BMI calculator.
+// Write a function that outputs (returns) a different message depending on the BMI.
+// The message MUST be returned as an output from your function. You should NOT NEED to use alert, prompt or console.log in this challenge.
+
+function bmiCalculator (weight, height) {
+    var bmi = weight / Math.pow(height, 2);
+    var interpretation = "";
+    if (bmi < 18.5) {
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+    }
+    if (bmi >= 18.5 && bmi <= 24.9) {
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+    }
+    if (bmi > 24.9) {
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+    return interpretation;
+}
+
+bmiCalculator (64, 1.75)

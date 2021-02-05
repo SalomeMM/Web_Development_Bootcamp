473e9b2d67c0a8f7302fbd52f73408b21a712b09
SalomeMM <sal.mm3@gmail.com>
2021-02-05 19:03:13 +0100

    check user answer with game pattern, works

--- 08. Simon Game with jQuery/game.js
+++ 08. Simon Game with jQuery/game.js
@@ -9,21 +9,21 @@ var started = false; // keep track of whether the game has started or not, so we
 var level = 0; // will show the level and it starts at level 0
 
 
-$(document).keypress(function() { // detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence()
-  
+$(document).keypress(function () { // detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence()
+
     if (!started) { // if started is false, which is, because that is how we built it to begin
 
-    $("#level-title").text("Level " + level); // will change h1 to "Level 0" when the game starts
-    nextSequence(); // calls the function that generates a random colour
-    started = true; // change started status from false to true
-  }
+        $("#level-title").text("Level " + level); // will change h1 to "Level 0" when the game starts
+        nextSequence(); // calls the function that generates a random colour
+        started = true; // change started status from false to true
+    }
 });
 
 
-$(".btn").click(function() { // will detect when any of the buttons are clicked and trigger a handler function
+$(".btn").click(function () { // will detect when any of the buttons are clicked and trigger a handler function
 
     var userChosenColour = $(this).attr("id"); // userChosenColour will store the id of the button that got clicked (this)
-  
+
     userClickedPattern.push(userChosenColour);
 
     // var audio = new Audio("sounds/" + userChosenColour + ".mp3"); // play a sound for the clicked colour
@@ -31,14 +31,44 @@ $(".btn").click(function() { // will detect when any of the buttons are clicked 
     playSound(userChosenColour);
 
     animatePress(userChosenColour); // will add .pressed class to the chosen colour
-  
-    console.log("userClickedPatter: " + userClickedPattern);
-  
-  });
+
+    checkAnswer(userClickedPattern.length - 1); // call after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
+
+    console.log("userClickedPatter: " + userClickedPattern.length);
+
+});
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) { // check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+
+        console.log("success");
+        
+        if (userClickedPattern.length === gamePattern.length) { // if the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+
+            setTimeout(function () { // call nextSequence() after a 1000 millisecond delay.
+                nextSequence();
+            }, 1000);
+
+        }
+
+    } else {
+
+        console.log("wrong");
+
+    }
+
+}
+
+
+
+
+
 
 function nextSequence() { // will generate the next colour
 
-    var randomNumber = Math.floor(Math.random()*4); // create a random number 0-3
+    var randomNumber = Math.floor(Math.random() * 4); // create a random number 0-3
 
     var randomChosenColour = buttonColours[randomNumber]; // select a colour from the array according to the random number
 
@@ -57,12 +87,12 @@ function nextSequence() { // will generate the next colour
 function playSound(name) { // creates a sound for a specific colour. We will call it when a random colour is selected and when a user clicks on a chosen button.
     var audio = new Audio("sounds/" + name + ".mp3"); // play a sound for the clicked colour
     audio.play();
-  };
-  
+};
+
 
 function animatePress(currentColour) { // will animate the pressed colour
     $("#" + currentColour).addClass("pressed"); // gives the pressed colour a new class: ".pressed"
     setTimeout(function () {
         $("#" + currentColour).removeClass("pressed"); // will removed the given class after 100 miliseconds = 0.1 seconds
-      }, 100);
+    }, 100);
 }

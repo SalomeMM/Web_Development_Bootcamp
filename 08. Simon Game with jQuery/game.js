80f40e01423f276177435803db9718f14c6109e9
SalomeMM <sal.mm3@gmail.com>
2021-02-05 18:52:03 +0100

    game start added

--- 08. Simon Game with jQuery/game.js
+++ 08. Simon Game with jQuery/game.js
@@ -4,6 +4,21 @@ var gamePattern = []; // empty array to which we will push the new generated col
 
 var userClickedPattern = []; // will store the userChosenColour every time
 
+var started = false; // keep track of whether the game has started or not, so we only call nextSequence() on the first keydown.
+
+var level = 0; // will show the level and it starts at level 0
+
+
+$(document).keypress(function() { // detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence()
+  
+    if (!started) { // if started is false, which is, because that is how we built it to begin
+
+    $("#level-title").text("Level " + level); // will change h1 to "Level 0" when the game starts
+    nextSequence(); // calls the function that generates a random colour
+    started = true; // change started status from false to true
+  }
+});
+
 
 $(".btn").click(function() { // will detect when any of the buttons are clicked and trigger a handler function
 
@@ -38,9 +53,6 @@ function nextSequence() { // will generate the next colour
     playSound(randomChosenColour);
 
 }
-
-nextSequence();
-
 
 function playSound(name) { // creates a sound for a specific colour. We will call it when a random colour is selected and when a user clicks on a chosen button.
     var audio = new Audio("sounds/" + name + ".mp3"); // play a sound for the clicked colour

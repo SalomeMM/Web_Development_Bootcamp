196a190b3e37d2ea6be1ff2a0f8aa70161cd11e5
SalomeMM <sal.mm3@gmail.com>
2021-02-14 16:35:51 +0100

    dirname

--- 10. Calculator/calculator.js
+++ 10. Calculator/calculator.js
@@ -1,9 +1,12 @@
+//jshint esversion:6
+
 const express = require("express");
 
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Hello Calculator");
+  res.sendFile(__dirname); // sendFile transfers the file over to the file when a request is made.
+// __dirname gives the file path of the current file no matter where it is hosted
 });
 
 app.listen(3000, function() {

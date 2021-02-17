0c83112085eda0b80c26560c9f87e01f4967ea13
SalomeMM <sal.mm3@gmail.com>
2021-02-17 19:03:31 +0100

    gitignore key added

--- 11_WeatherProject/app.js
+++ 11_WeatherProject/app.js
@@ -1,14 +1,16 @@
 const express = require("express"); // require express
 const https = require("https");
+const url = require("./keys").url; // import weather api key from git-ignored file
+
 
 const app = express(); // initialize a new express app
 
 app.get("/", function (req, res) { // what happens when the user tries to go to my home page
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=57b3808da7a08764de3a87288de77391&units=metric";
+    // without gitignore, we would declare the variable "url" here, with the api key
 
     https.get(url, function (response) {
-        console.log(response);
+        console.log("hey" + response.statusCode);
     });
 
     res.send("Server is up and running")

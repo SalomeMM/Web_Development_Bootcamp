a1e156c6c97e897f32638e962312602954f69eaf
SalomeMM <sal.mm3@gmail.com>
2021-02-18 16:04:27 +0100

    res sent

--- 11_WeatherProject/app.js
+++ 11_WeatherProject/app.js
@@ -6,20 +6,32 @@ const url = require("./keys").url; // import weather api key from git-ignored fi
 const app = express(); // initialize a new express app
 
 app.get("/", function (req, res) { // what happens when the user tries to go to my home page
-
     // without gitignore, we would declare the variable "url" here, with the api key
 
     https.get(url, function (response) {
         console.log("status code: " + response.statusCode);
 
-        response.on("data", function(data){
-// console.log(data); //will show hexadecimal code for the json data. It would be more useful to have a JS object, like this:
-const weatherData = JSON.parse(data)
-console.log(weatherData);
+        response.on("data", function (data) {
+            // console.log(data); //will show hexadecimal code for the json data. It would be more useful to have a JS object, like this:
+            const weatherData = JSON.parse(data) // recreate js object with JSON.parse.
+            console.log(weatherData);
+            // const exampleObject = {
+            //     name: "Jane",
+            //     favouriteFood: "Sushi"
+            // }
+            // JSON.stringify(exampleObject); // does the opposite of JSON.parse: turn js object into a string.
+            // console.log(exampleObject);
+
+            const temp = weatherData.main.temp; // extract specific pieces of information, e.g. temperature. chrome extension "json viewer pro" HELPS getting full paths.
+            console.log(temp);
+
+            const weatherDescription = weatherData.weather[0].description; // extract specific pieces of information, e.g. temperature. chrome extension "json viewer pro" HELPS getting full paths.
+            console.log(weatherDescription);
+            res.send("The temperature is " + temp + "°C.")
         })
     });
 
-    res.send("Server is up and running")
+    // res.send("Server is up and running") // there can only be 1 res.send in any app method.
 })
 
 app.listen(3000, function () {

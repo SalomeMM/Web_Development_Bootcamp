55f2df27778d8b7678576e05e8847b04ae5c2208
SalomeMM <sal.mm3@gmail.com>
2021-02-20 16:20:09 +0100

    server up and running

--- 12_Newsletter-Signup/app.js
+++ 12_Newsletter-Signup/app.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Server up and running")
+});
+
+app.listen(3000, function(){
+    console.log("The server is running on port 3000");
+})

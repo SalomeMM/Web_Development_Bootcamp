4b8d9781a986916df1f5331f79ce351fb1d2d4ca
SalomeMM <sal.mm3@gmail.com>
2021-02-20 16:30:41 +0100

    jshint 6

--- 12_Newsletter-Signup/app.js
+++ 12_Newsletter-Signup/app.js
@@ -2,14 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request")
+const request = require("request");
 
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Server up and running")
+    res.send("Server up and running");
 });
 
 app.listen(3000, function(){
     console.log("The server is running on port 3000");
-})
+});

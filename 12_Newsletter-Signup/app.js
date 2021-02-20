d30f0131af026a9a32a9c11e2889f5603e1ab080
SalomeMM <sal.mm3@gmail.com>
2021-02-20 17:29:10 +0100

    public folder / express.static

--- 12_Newsletter-Signup/app.js
+++ 12_Newsletter-Signup/app.js
@@ -6,6 +6,8 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public")); //special express function that creates a static folder (public) and provides the path to it.
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });

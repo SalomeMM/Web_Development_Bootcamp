0ef55b8b5fcb88de694fe4858b8a3da9309552d1
SalomeMM <sal.mm3@gmail.com>
2021-02-20 17:39:22 +0100

    post request working

--- 12_Newsletter-Signup/app.js
+++ 12_Newsletter-Signup/app.js
@@ -7,9 +7,20 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public")); //special express function that creates a static folder (public) and provides the path to it.
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+var firstName = req.body.fName;
+var lastName = req.body.lName;
+var email = req.body.email;
+
+console.log("Nombre: " + firstName, "Apellido: " + lastName, "Email: " + email);
+
 });
 
 app.listen(3000, function(){

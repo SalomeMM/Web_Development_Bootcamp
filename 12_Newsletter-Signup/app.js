f29e5cbdbf4c584a134448bd30cc20863141c88d
SalomeMM <sal.mm3@gmail.com>
2021-02-21 20:10:38 +0100

    posts new user

--- 12_Newsletter-Signup/app.js
+++ 12_Newsletter-Signup/app.js
@@ -4,60 +4,69 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const request = require("request");
-const { post } = require("request");
+const {
+    post
+} = require("request");
 
 const app = express();
 
 app.use(express.static("public")); //special express function that creates a static folder (public) and provides the path to it.
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
-const firstName = req.body.fName;
-const lastName = req.body.lName;
-const email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-const data = {
-    members: [
-        {
-            email_address: email, 
+    const data = {
+        members: [{
+            email_address: email,
             status: "subscribed",
             merge_fields: {
                 FNAME: firstName,
                 LNAME: lastName
             }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data); // turn these data into a string
+
+    const url = "https://us1.api.mailchimp.com/3.0/lists/ec795f4281";
+    // server = us1.api...
+
+    const options = {
+        method: "POST",
+        auth: "salome1:825ce866b31357b837e36d4fadd55bcf-us1",
+    };
+
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.send("Successfully subscribed!");
+        } else {
+            res.send("There was an error with signing up, please try again!");
         }
-    ]
-};
 
-const jsonData = JSON.stringify(data); // turn these data into a string
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        });
+    });
 
-const url = "https://us1.api.mailchimp.com/3.0/lists/ec795f4281";
-// server = us1.api...
-
-const options = {
-    method: "POST",
-    auth: "salome1:825ce866b31357b837e36d4fadd55bcf-us1",
-};
-
-const request = https.request(url, options, function(response){
-response.on("data", function(data){
-    console.log(JSON.parse(data));
-});
-});
-
-request.write(jsonData);
-request.end();
+    request.write(jsonData);
+    request.end();
 
 });
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("The server is running on port 3000");
 });
 

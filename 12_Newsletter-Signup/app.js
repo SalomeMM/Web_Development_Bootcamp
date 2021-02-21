d05964974be782782491a3cd4239fdd09e6d7bb9
SalomeMM <sal.mm3@gmail.com>
2021-02-21 20:05:58 +0100

    api working, posts new subscribers

--- 12_Newsletter-Signup/app.js
+++ 12_Newsletter-Signup/app.js
@@ -1,8 +1,10 @@
 //jshint esversion: 6
 
 const express = require("express");
+const https = require("https");
 const bodyParser = require("body-parser");
 const request = require("request");
+const { post } = require("request");
 
 const app = express();
 
@@ -15,14 +17,50 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-var firstName = req.body.fName;
-var lastName = req.body.lName;
-var email = req.body.email;
+const firstName = req.body.fName;
+const lastName = req.body.lName;
+const email = req.body.email;
 
-console.log("Nombre: " + firstName, "Apellido: " + lastName, "Email: " + email);
+const data = {
+    members: [
+        {
+            email_address: email, 
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }
+    ]
+};
+
+const jsonData = JSON.stringify(data); // turn these data into a string
+
+const url = "https://us1.api.mailchimp.com/3.0/lists/ec795f4281";
+// server = us1.api...
+
+const options = {
+    method: "POST",
+    auth: "salome1:825ce866b31357b837e36d4fadd55bcf-us1",
+};
+
+const request = https.request(url, options, function(response){
+response.on("data", function(data){
+    console.log(JSON.parse(data));
+});
+});
+
+request.write(jsonData);
+request.end();
 
 });
+
+
 
 app.listen(3000, function(){
     console.log("The server is running on port 3000");
 });
+
+
+// API Key: 825ce866b31357b837e36d4fadd55bcf-us1
+// unique id / audience id: ec795f4281

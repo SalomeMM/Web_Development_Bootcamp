534dab671b63bcec7e90d2937c9738b373cbeb01
SalomeMM <sal.mm3@gmail.com>
2021-03-03 18:00:14 +0100

    home render

--- 14_Blog_Project/app.js
+++ 14_Blog_Project/app.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 
-const express = require("express");
+const express = require("express"); // import modules
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
@@ -8,22 +8,16 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
+const app = express(); // create express app
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // set view engine using ejs
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true})); // use body parser
+app.use(express.static("public")); // tell express that our static files are held inside the public folder
 
-
-
-
-
-
-
-
-
-
+app.get("/", function (req,res) {
+  res.render("home"); // the page we want to render needs to be something .ejs and be inside a folder called views in the root of our maijn project in the same hierarchical level as app.js 
+})
 
 
 

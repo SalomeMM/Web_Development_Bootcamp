1283caf49381be428b1c3dde84310dbf0bb4c876
SalomeMM <sal.mm3@gmail.com>
2021-03-06 12:01:06 +0100

    fix deprecation warning

--- 15_Fruits_Project/app.js
+++ 15_Fruits_Project/app.js
@@ -8,12 +8,13 @@ const url = 'mongodb://localhost:27017'; // standard port for mongo
 const dbName = 'fruitsDB'; // if this DB doesn't exist, it will create it
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
   assert.equal(null, err);
-  console.log("Connected successfully to server");
+    console.log("Connected successfully to server");
+    // if this fails, get mongodb server up and running with "mongod"
 
   const db = client.db(dbName);
 

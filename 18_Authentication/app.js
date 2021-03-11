680d6baf1aa4fd7af7ffd455736200d926c1644f
SalomeMM <sal.mm3@gmail.com>
2021-03-11 16:56:35 +0100

    Level 3 - Hashing Passwords with md5

--- 18_Authentication/app.js
+++ 18_Authentication/app.js
@@ -6,7 +6,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption"); // can also authenticate
+// const encrypt = require("mongoose-encryption"); // can also authenticate // substituted by md5
+const md5 = require("md5");
 
 const app = express();
 
@@ -30,7 +31,7 @@ const userSchema = new mongoose.Schema ({
   password: String,
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); // substituted by md5
 
 // create this before the mongoose model
 
@@ -51,7 +52,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password)
   });
   newUser.save(function (err) { // mongoose-encryption will encrypt the password
     if (err) {
@@ -64,7 +65,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const username = req.body.username; // the one that the user types when trying to log in
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({ email: username }, function (err, foundUser) { //find the user in our DB, mongoose-encryption will decrypt the password
         if (err) {
             console.log(err);

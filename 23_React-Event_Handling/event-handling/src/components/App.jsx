0d06650cdda2c84f2e878d40599ca2197a256c33
SalomeMM <sal.mm3@gmail.com>
2021-03-15 16:01:28 +0100

    conditional rendering, setState, useState

--- 23_React-Event_Handling/event-handling/src/components/App.jsx
+++ 23_React-Event_Handling/event-handling/src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello"); // Hello is the starting value
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+    console.log("clicked");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+      setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }

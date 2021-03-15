d2f913ca70e26d27ed9470ee855cc0ca7c1c2ecc
SalomeMM <sal.mm3@gmail.com>
2021-03-15 15:42:42 +0100

    starting files

---
+++ 23_React-Event_Handling/event-handling/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default App;

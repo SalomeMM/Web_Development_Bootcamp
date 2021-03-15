99a64b5fca04883de639b72cd95b6c32b629419b
SalomeMM <sal.mm3@gmail.com>
2021-03-15 14:16:12 +0100

    conditional rendering

--- 19_React_Playground/react-playground/src/components/App.jsx
+++ 19_React_Playground/react-playground/src/components/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import Login from "./Login";
 
-var isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+const currentTime = new Date(2019, 11, 1, 9).getHours();
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return <div className="container">{
+    currentTime > 12 && <h1>Why are you still working?</h1>
+    /* currentTime > 12 ? <h1>Why are you still working?</h1> : null */
+    // render is condition is met, don`t (null) if it's not
+    
+    }</div>;
 }
 
 export default App;

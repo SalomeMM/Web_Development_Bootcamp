e295ed16fade273ceb42135d8f5cbb8b0008430b
SalomeMM <sal.mm3@gmail.com>
2021-03-16 10:19:31 +0100

    manage fName and lName separately

--- 25_React-Changing_Complex_States/changing-complex-states/src/components/App.jsx
+++ 25_React-Changing_Complex_States/changing-complex-states/src/components/App.jsx
@@ -2,25 +2,50 @@ import React, { useState } from "react";
 
 function App() {
 
-    const [fName, setFName] = useState("");
-    const [lName, setLName] = useState("");
+  const [fullName, setFullName] = useState({ // our fullName is storing an object
+    fName: "",
+    lName: ""
+  });
+  
+  // when any of the inputs is changed, they call one single function, passing over the event that causes it
+  // inside the function we get a new value (change), but want to keep the old values of the object, to change only 1
 
-    function updateFName(event) {
-        const firstName = event.target.value;
-        setFName(firstName);
-    }
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
 
-    function updateLName(event) {
-        const lastName = event.target.value;
-        setLName(lastName);
-    }
+    setFullName((prevValue) => {
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        }
+      }
+      console.log(prevValue);
+    });
+  }
 
   return (
     <div className="container">
-          <h1>Hello {fName} {lName}</h1>
+          <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
-        <input name="fName" onChange={updateFName} placeholder="First Name" value={fName} />
-        <input name="lName" onChange={updateLName} placeholder="Last Name" value={lName} />
+        <input
+          name="fName"
+          onChange={handleChange}
+          placeholder="First Name"
+          value={fullName.fName}
+          />
+        <input
+          name="lName"
+          onChange={handleChange}
+          placeholder="Last Name"
+          value={fullName.lName}
+          />
         <button>Submit</button>
       </form>
     </div>

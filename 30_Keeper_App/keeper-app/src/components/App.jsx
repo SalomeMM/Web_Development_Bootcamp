c0216f938b508fa0697bae89df2db0697e7b3689
SalomeMM <sal.mm3@gmail.com>
2021-03-18 16:13:49 +0100

    implement the delete note functionality

--- 30_Keeper_App/keeper-app/src/components/App.jsx
+++ 30_Keeper_App/keeper-app/src/components/App.jsx
@@ -1,29 +1,44 @@
 //jshint esversion:6
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-
   //3. Add notes to an array
   const [notes, setNotes] = useState([]); // starts with no notes and gets populated when the user adds them
 
   function addNote(newNote) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return [...prevNotes, newNote]; // get hold of previous notes and add the new one at the end
+    });
+  }
+
+  function deleteNote(id) { // this function is passed to all rendered notes as a property
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => { // loop through the previous notes
+        return index !== id; // returns all notes where the index does not equal the id of the note that needs to be deleted
+      }) 
     })
-}
+
+  }
 
   return (
     <div>
       <Header />
-      <CreateArea
-        onAdd={addNote} />
-      {notes.map((noteItem) => {
-        return <Note title={noteItem.title} content={noteItem.content} />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index} // will pick this up in Note.jsx as props for onDelete()
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       {/* <Note key={1} title="Note title" content="Note content" /> //static-note */}
       <Footer />
--- 30_Keeper_App/keeper-app/src/components/Note.jsx
+++ 30_Keeper_App/keeper-app/src/components/Note.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Note(props) {
+  function handleClick() {
+    // gets passed over from the props and will delete this this note from the array
+    props.onDelete(props.id); // trigger onDelete() to delete the note when clicking the delete button
+  }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>DELETE</button>
+      <button onClick={handleClick}>DELETE</button>
     </div>
   );
 }
